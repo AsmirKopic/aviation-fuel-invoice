669f97448e682729ea69de1a5357784e1b08a8f6
Asmir Kopic <asmir.kopic123@gmail.com>
2022-02-02 23:11:01 +0100

    setup remote database connection

--- src/main/java/com/aviationfuelinvoiceapp/aviationfuelinvoice/AviationFuelInvoiceApplication.java
+++ src/main/java/com/aviationfuelinvoiceapp/aviationfuelinvoice/AviationFuelInvoiceApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication  //(exclude = SecurityAutoConfiguration.class)
-public class AviationFuelInvoiceApplication {
+public class AviationFuelInvoiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AviationFuelInvoiceApplication.class, args);
--- src/main/java/com/aviationfuelinvoiceapp/aviationfuelinvoice/controller/DashboardController.java
+++ src/main/java/com/aviationfuelinvoiceapp/aviationfuelinvoice/controller/DashboardController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
 public class DashboardController {
 
     InvoiceService invoiceService;
